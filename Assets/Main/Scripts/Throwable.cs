0ff7ea8fe16834da3699efea50a9f468a1075874
Matthias De Geyter <matthias.degeyter@imec.be>
2017-01-18 13:59:11 +0100

    working throwable

--- Assets/Main/Scripts/Throwable.cs
+++ Assets/Main/Scripts/Throwable.cs
@@ -5,18 +5,19 @@ using System;
 
 public class Throwable : MonoBehaviour {
 
-	public static event Action<int,Throwable> onPlayerPickupEnter;
-	public static event Action<int, Throwable> onPlayerPickupExit;
-
-	public Transform player1;
-	public Transform player2;
+	public static event Action<int,Throwable> onPlayerPickupEnter = delegate{};
+	public static event Action<int, Throwable> onPlayerPickupExit = delegate {};
 
 	Collider2D _triggerCollider;
 	Collider2D _throwableCollider;
 	Rigidbody2D _rb2D;
+	Color _originalColor;
+	SpriteRenderer _renderer;
 
 	// Use this for initialization
 	void Start () {
+		_renderer = GetComponent<SpriteRenderer> ();
+		_originalColor = _renderer.color;
 		Collider2D[] _collider2D = GetComponents<Collider2D> ();
 
 		if (_collider2D [0].isTrigger) {
@@ -27,6 +28,8 @@ public class Throwable : MonoBehaviour {
 			_triggerCollider = _collider2D [1];
 		}
 		_rb2D = GetComponent<Rigidbody2D> ();
+
+		Debug.Log ("test");
 	}
 	
 	// Update is called once per frame
@@ -37,11 +40,12 @@ public class Throwable : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
+		Debug.Log (col.ToString());
 		if (col.CompareTag ("Player")) {
-			//highlight
+			_renderer.color = Color.white;
 			//todo
 
-			Player player = col.gameObject.GetComponent<Player> ();
+			Player player = col.transform.parent.gameObject.GetComponent<Player> ();
 			onPlayerPickupEnter (player.playerNumber,this);
 
 		}
@@ -49,10 +53,10 @@ public class Throwable : MonoBehaviour {
 	void OnTriggerExit2D(Collider2D col)
 	{
 		if (col.CompareTag ("Player")) {
-			//highlight
+			//unhighlight
 			//todo
-
-			Player player = col.gameObject.GetComponent<Player> ();
+			_renderer.color = _originalColor;
+			Player player = col.transform.parent.gameObject.GetComponent<Player> ();
 			onPlayerPickupExit (player.playerNumber,this);
 
 		}
