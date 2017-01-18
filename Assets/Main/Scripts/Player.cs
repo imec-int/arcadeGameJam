d5de69d13534dd5d034f5bf980a48c85a7dd82f7
Robby Wauters <robbywauters@gmail.com>
2017-01-18 11:26:45 +0100

    basic 2D game

--- Assets/Main/Scripts/Player.cs
+++ Assets/Main/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System;
 using System.Collections.Generic;
+using Prime31;
 
 public class Player : MonoBehaviour
 {
@@ -11,8 +12,58 @@ public class Player : MonoBehaviour
 	}
 
 	public int playerNumber;
+	// movement config
+	public float gravity = -25f;
+	public float runSpeed = 8f;
+	public float groundDamping = 20f; // how fast do we change direction? higher means faster
+	public float inAirDamping = 5f;
+	public float jumpHeight = 3f;
 
+	[HideInInspector]
+	private float normalizedHorizontalSpeed = 0;
+
+	private CharacterController2D _controller;
+	private Animator _animator;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
 	Dictionary<Button,string> _buttons;
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_controller = GetComponent<CharacterController2D>();
+
+		// listen to some events for illustration purposes
+		_controller.onControllerCollidedEvent += onControllerCollider;
+		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+		_controller.onTriggerExitEvent += onTriggerExitEvent;
+	}
+
+	#region Event Listeners
+
+	void onControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits cause they arent very interesting
+		if( hit.normal.y == 1f )
+			return;
+
+		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+	}
+
+
+	void onTriggerEnterEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerEnterEvent: " + col.gameObject.name );
+	}
+
+
+	void onTriggerExitEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+
+	#endregion
 
 	void Start()
 	{
@@ -31,31 +82,67 @@ public class Player : MonoBehaviour
 
 	void Update ()
 	{
-		//Joystick directions
-		if( Input.GetButtonDown( _buttons[Button.UP] ) )
+		if( _controller.isGrounded )
+			_velocity.y = 0;
+
+		if( Input.GetButton( _buttons[Button.RIGHT] ) )
 		{
-			Debug.Log(_buttons[Button.UP]);
+			normalizedHorizontalSpeed = 1;
+			if( transform.localScale.x < 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+
+			if( _controller.isGrounded )
+				_animator.Play( Animator.StringToHash( "Run" ) );
 		}
-		if( Input.GetButtonDown( _buttons[Button.DOWN] ) )
+		else if( Input.GetButton( _buttons[Button.LEFT] ) )
 		{
-			Debug.Log(_buttons[Button.DOWN]);
+			normalizedHorizontalSpeed = -1;
+			if( transform.localScale.x > 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+
+			if( _controller.isGrounded )
+				_animator.Play( Animator.StringToHash( "Run" ) );
 		}
-		if( Input.GetButtonDown( _buttons[Button.LEFT] ) )
+		else
 		{
-			Debug.Log(_buttons[Button.LEFT]);
-		}
-		if( Input.GetButtonDown( _buttons[Button.RIGHT] ) )
-		{
-			Debug.Log(_buttons[Button.RIGHT]);
+			normalizedHorizontalSpeed = 0;
+
+			if( _controller.isGrounded )
+				_animator.Play( Animator.StringToHash( "Idle" ) );
 		}
 
-		//Action buttons
-		if( Input.GetButtonDown( _buttons[Button.A] ) )
+
+		// we can only jump whilst grounded
+		if( _controller.isGrounded && Input.GetButtonDown( _buttons[Button.A] ) )
 		{
-			Debug.Log(_buttons[Button.A]);
+			_velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
+			_animator.Play( Animator.StringToHash( "Jump" ) );
 		}
+
+
+		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
+
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+
+		// if holding down bump up our movement amount and turn off one way platform detection for a frame.
+		// this lets us jump down through one way platforms
+		if( _controller.isGrounded && Input.GetButton( _buttons[Button.DOWN] ) )
+		{
+			_velocity.y *= 3f;
+			_controller.ignoreOneWayPlatformsThisFrame = true;
+		}
+
+		_controller.move( _velocity * Time.deltaTime );
+
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller.velocity;
+
 		if( Input.GetButtonDown( _buttons[Button.B] ) )
 		{
+			//TODO: b-button stuff!
 			Debug.Log(_buttons[Button.B]);
 		}
 	}
