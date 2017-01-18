81acf08a17ea5eafe4582c7f153008085703fe22
Miel Vander Sande <miel.vandersande@ugent.be>
2017-01-18 15:30:18 +0100

    Added hud

---
+++ Assets/Main/Scripts/Hud.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hud : MonoBehaviour
+{
+	public GameObject win;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
